7abb04b03406d45ff5d6e451eb40aa14aca148cc
Guido Serio <gserio@olon.it>
2023-08-07 23:08:12 +0200

    fixed sorting of blog posts

--- gulp/tasks/generateDataFiles.js
+++ gulp/tasks/generateDataFiles.js
@@ -9,9 +9,18 @@ gulp.task('generateDataFiles', function (done) {
   const dataDirectory = config.src.njDataDir;
 
   const blogContent = JSON.parse(fs.readFileSync(contentPath));
+const sortedPosts = Object.entries(blogContent)
+  .sort(([keyA, postA], [keyB, postB]) => new Date(postB.date) - new Date(postA.date))
+  .reduce((acc, [key, post]) => ({ ...acc, [key]: post }), {});
 
-  Object.keys(blogContent).forEach((postName) => {
+
+
+
+
+  Object.keys(sortedPosts).forEach((postName) => {
     const postData = blogContent[postName];
+
+	console.log(postData.date);
 	
 	let regex = /\n/g;
 	let replacedText = blogContent[postName].body.replace(regex, "\\n");
@@ -21,6 +30,7 @@ gulp.task('generateDataFiles', function (done) {
     const dataFileName = postName.replace('.json', '') + '.html';
     const dataFilePath = path.join(dataDirectory, dataFileName);
     const templateData = `{% set list = [${JSON.stringify(postData)}] %}`;
+
 
     fs.writeFileSync(dataFilePath, templateData);
 
